70ccb402417657da5453472943df651f69236a1f
Antonio Rufino <devs.toni@gmail.com>
2023-03-28 00:23:26 +0200

    Add toast when form is submitted

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import { helpHttp } from '../helpers/helpHttp';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
+const MySwal = withReactContent(Swal)
 
 export const useForm = (initialForm) => {
 
@@ -27,15 +30,30 @@ export const useForm = (initialForm) => {
         Accept: 'application/json'
       }
     }).then(res => {
-      console.log(res)
       setLoading(false);
       setResponse(res);
+      Toast.fire({
+        icon: 'success',
+        title: 'Email sent successfully!'
+      })
       setTimeout(() => {
         setResponse(false)
       }, 5000);
       setForm(initialForm);
     })
   };
+
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
 
   return {
     form, errors, loading, response, handleChange, handleSubmit
